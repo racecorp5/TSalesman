601dcd002e4ad5a8211369070c270585b1a2ecfe
racecorp5 <racecorp5@gmail.com>
2024-11-20 00:15:46 -0500

    fix

--- frontend/replace-env.sh
+++ frontend/replace-env.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-sed -i "s/GMAPS_API_KEY/${GMAPS_API_KEY}/g" ./src/index.html
+if [ "$(uname)" = "Darwin" ]; then
+  # macOS
+  sed -i '' "s/GMAPS_API_KEY/${GMAPS_API_KEY}/g" ./src/index.html
+else
+  # Linux
+  sed -i "s/GMAPS_API_KEY/${GMAPS_API_KEY}/g" ./src/index.html
+fi
